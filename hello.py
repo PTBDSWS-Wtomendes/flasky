c27bc26816b43f671f33ff11798c69b94e8a43b8
Wellington <wellingtonjrcmendes@gmail.com>
2026-08-02 21:22:16 -0300

    rotas

--- hello.py
+++ hello.py
@@ -1,10 +1,51 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from flask import Flask, request, make_response, redirect, abort
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+    return '''
+    <p>Alterações por meio do PythonAnywhere -> GitHub</p>
+    <table border="1">
+        <tr>
+            <td><b>Professor:</b></td>
+            <td>Professor Fabio Teixeira</td>
+        </tr>
+        <tr>
+            <td><b>Prontuário:</b></td>
+            <td>PT23820X</td>
+        </tr>
+    </table>
+    '''
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return f'<h1>Hello, {name}!</h1>'
+
+
+@app.route('/contextorequisicao')
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent')
+    return f'<p>Seu navegador é: {user_agent}</p>'
+
+
+@app.route('/codigostatusdiferente')
+def codigostatusdiferente():
+    return "<h1>Status 201</h1>", 201
+
+
+@app.route('/objetoresposta')
+def objetoresposta():
+    resposta = make_response("<h1>Objeto Response</h1>")
+    resposta.headers["Professor"] = "Fabio Teixeira"
+    return resposta
+
+
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect('/')
+
+
+@app.route('/abortar')
+def abortar():
+    abort(404)
